663640eadcaf31aceefb9d2ae70c46bc08b490f2
adikakade31 <adi.kakade31@gmail.com>
2016-10-14 02:25:35 -0700

    Update Movie.java

--- java/com.codepath.flickster/Models/Movie.java
+++ java/com.codepath.flickster/Models/Movie.java
@@ -1,5 +1,8 @@
 package com.codepath.flickster.Models;
 
+import com.codepath.flickster.CommonUtils.CommonConstants;
+import com.codepath.flickster.CommonUtils.PopularityStatus;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,15 +12,24 @@ import java.util.ArrayList;
 /**
  * Created by aditikakadebansal on 10/11/16.
  */
+
+
 public class Movie {
+
+    private final int POPULARITY_THRESHOLD = 5;
+
     private String posterPath;
+    private String backdropPath;
     private String movieTitle;
     private String overview;
+    private int stars;
 
     public Movie(JSONObject jsonObject) throws JSONException{
         this.posterPath = jsonObject.getString("poster_path");
+        this.backdropPath = jsonObject.getString("backdrop_path");
         this.movieTitle = jsonObject.getString("original_title");
         this.overview = jsonObject.getString("overview");
+        this.stars = jsonObject.getInt("vote_average");
     }
 
     public static ArrayList<Movie> getMoviesFromJSONArray(JSONArray jsonArray){
@@ -32,8 +44,19 @@ public class Movie {
         return movieResults;
     }
 
+    public PopularityStatus getPopularity() {
+        if (this.stars >= POPULARITY_THRESHOLD) {
+            return PopularityStatus.POPULAR;
+        }
+        return PopularityStatus.UNPOPULAR;
+    }
+
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342%s",posterPath);
+    }
+
+    public String getBackdropPath(){
+        return String.format("https://image.tmdb.org/t/p/w342%s",backdropPath);
     }
 
     public String getMovieTitle() {
@@ -42,5 +65,9 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public int getStars(){
+        return stars;
     }
 }
